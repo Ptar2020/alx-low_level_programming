99eac16df273ce4faf7c2a00b90feefc17bd2758
Peter Okoth Olwande <peterolwande@gmail.com>
2022-06-30 13:01:45 -0700

    6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main -  Entry point
+ *
+ * Return:Always 0(Success)
+ */
+int main(void)
+{
+  int a;
+  long int b;
+  long long int c;
+  char d;
+  float e;
+  printf("%lu",sizeof(a));
+  printf("%lu",sizeof(b));
+  printf("%lu",sizeof(c));
+  printf("%lu",sizeof(d));
+  printf("%lu",sizeof(e));
+  return 0;
+}
