8a20c956e8c571e44893afadb1d5906c3d9f379e
Peter Okoth Olwande <peterolwande@gmail.com>
2022-06-30 12:43:01 -0700

    juuu

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -5,7 +5,5 @@
  * Retun:Always 0 (Success)
  */
 int main(void)
-{
-printf("with proper grammar, but the outcome is a piece of art,\n");
-return (0);
-}
+{printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);}
