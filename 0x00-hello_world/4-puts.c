03e39c8839c5f327c9db3a8855140574f24a485f
Peter Okoth Olwande <peterolwande@gmail.com>
2022-06-30 12:25:12 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+
 /**
  *main - Entry point
  *
@@ -7,7 +8,5 @@
 int main(void)
 {  
   puts("\"Programming is like building a multilingual puzzle");
-  
-  return (0);
-  
+  return (0);  
 }           
