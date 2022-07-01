893385beed81c76a2c9a3d00282045ea3d953729
Peter Okoth Olwande <peterolwande@gmail.com>
2022-07-01 12:37:28 -0700

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -5,35 +5,19 @@
  * Return: Always (Success);
  */
 
-int main(void)
-  
+int main(void) 
 {
- 
-  int c;
-   
-  for (c = '0'; c <= '9'; c++)
-    
-    {
+int c;  
+for (c = '0'; c <= '9'; c++)
+{
+putchar(c);
+}
       
-      putchar(c);
-   
-
-      
-      if (c != '9')
-	
-	{
-	  
-	  putchar(',');
-	  
-	  putchar(' ');
-	  
-	}
-      
-    }
-  
-
-  
-  putchar('\n');
-   
-  return (0);
- }
+if (c != '9')	
+{
+putchar(',');	  
+putchar(' ');	  
+}
+putchar('\n');
+return (0);
+}
