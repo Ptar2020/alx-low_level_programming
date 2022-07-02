56637348c4d4ae370c5347bca613cd971a161a33
Peter Okoth Olwande <peterolwande@gmail.com>
2022-07-01 23:02:22 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,17 @@
-#include <stdio.h>
+#include <unistd.h>
 /**
- * main Printing a text
- *
- *Return:Always 1(Success)
- */
+
+  * main - Prints a text
+  *
+  * Return: Always 1 (Success)
+  */
+
 int main(void)
+
 {
-  write(2,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-  return (1);
+
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+
+	return (1);
+
 }
