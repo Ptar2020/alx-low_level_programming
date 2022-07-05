c39b33b7ecdad831d37c76c6b9849198938d0e77
Peter Okoth Olwande <peterolwande@gmail.com>
2022-07-05 11:23:36 -0700

    hut off

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,17 +1,17 @@
 #include "main.h"
 
 /**
- * add
+ * add - the entry to the function
  *
  * @a one of the numbers
  * @b the other number
  * @ans the answer returned
- * Return
+ * Return: the value of the addition
  */
 
 int add(int a, int b)
 {
 int ans;
 ans = a + b;
-return ans;
+return (ans);
 }
