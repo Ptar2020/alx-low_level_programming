90ff795a5cd433d4beb6babf94b44ac684f8d793
Peter Okoth Olwande <peterolwande@gmail.com>
2022-07-06 08:20:50 -0700

    gets

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -9,13 +9,16 @@
 int main(void)
 {
 int i;
+
 printf("infinite loop incoming :(\n");
 
 i = 0;
+
 /**while (i< 10)
  *{
  *putchar(i);
  *}*/
 printf("Infinite loop avoided! \\o/\n");
+
 return (0);
 }
