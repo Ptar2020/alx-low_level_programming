aed4b2ebe33fc57f2d9549f70df18db525687c7f
Ptar2020 <peterolwande@gmail.com>
2022-07-07 08:23:06 +0300

    2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * mul entry point
+ *
+ * @a: One of the variables
+ * @b: The other variable
+ * Return: The result of the multiplication
+ */
+int mul(int a, int b)
+{
+    return (a * b);
+}
+
