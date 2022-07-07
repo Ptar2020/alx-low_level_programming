4a4e054efd22062bb527ee9f2a4b453ce1474b71
Ptar2020 <peterolwande@gmail.com>
2022-07-07 07:43:27 +0300

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+*
+* isupper - the main function
+*
+* Return:The return of the function
+*/
+
+int isupper(int c)
+{
+    c = 65;
+    while (c <= 90)
+    {
+        return (1);
+        c++;
+    }
+    return (0);
+}
