b53ae82479fde5d1aeb0d81e02f317c945c1c8a1
Peter Okoth Olwande <peterolwande@gmail.com>
2022-07-08 13:00:43 -0700

    0-main.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,17 +1,17 @@
 #include "main.h"
 
 /**
-*
 * _isupper - the main function
 *
+* @c the variable passed
 * Return:The return of the function
 */
 
 int _isupper(int c)
 {
-    if (c <= 65 && c >= 90)
-    {
-        return (1);
-    }
-    return (0);
+if (c <= 65 && c >= 90)
+{
+	return (1);
+}
+	return (0);
 }
