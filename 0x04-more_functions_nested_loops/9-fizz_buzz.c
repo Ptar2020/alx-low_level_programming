5562b9476119138e9f0c0270481539952bbc64a6
Peter Okoth Olwande <peterolwande@gmail.com>
2022-07-10 10:40:42 +0300

    9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,43 +1,42 @@
 #include "main.h"
 #include <stdio.h>
 /**
-* fizz_buzz - function call
+* main - function call
 *
 * @a - The Uused parameter
 * Return: the returned numbers as per the given conditions
 */
 
-int main()
+int main(void)
 {
-    int a = 1;
+	int a = 1;
 
-
-    while ( a <= 100)
-    {
-        int i = 1;
-        if (i % 3 == 0)
-        {
-            if  (i % 5 == 0)
-            {
-                printf("FizzBuzz");
-            }
-            else
-            {
-                printf("Fizz");
-            }
-        }
-        if (i % 5 == 0)
-        {
-            if  (i % 3 == 0)
-            {
-                printf("FizzBuzz");
-            }
-            else
-            {
-                printf("Buzz");
-            }
-        }
-        a++;
-    }
-    return (0);
+	while ( a <= 100)
+	{
+		int i = 1;
+		if (i % 3 == 0)
+		{
+			if  (i % 5 == 0)
+			{
+				printf("FizzBuzz");
+			}
+			else
+			{
+				printf("Fizz");
+			}
+		}
+		if (i % 5 == 0)
+		{
+			if  (i % 3 == 0)
+			{
+				printf("FizzBuzz");
+			}
+			else
+			{
+				printf("Buzz");
+			}
+		}
+		a++;
+	}
+	return (0);
 }
