5461593ef5f97fe662c4842130df2866c3b51e2a
Peter Okoth Olwande <peterolwande@gmail.com>
2022-07-11 11:38:40 +0300

    0-98

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,8 +1,9 @@
 #include "main.h"
 /**
- * reset_to_98 the executing function entry
+ * reset_to_98 - the executing function entry
  *
  * @n: The pointer to variable n
+ * return void
  *
  */
 void reset_to_98(int *n)
