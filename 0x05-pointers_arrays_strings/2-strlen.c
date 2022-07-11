7fa2139d01900feb49d34b59b0f6778d00d9d23a
Peter Okoth Olwande <peterolwande@gmail.com>
2022-07-11 16:14:29 +0300

    2-strlen.c

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -5,17 +5,19 @@
  * _strlen - works ou the length of the string given
  *
  * @s: The string to find its length
- * return length of the string
+ * @c the returned value
+ * return: length of the string
  *
  */
 
 int _strlen(char *s)
 {
 	int c = 0;
+
 	for (; *s != '\0'; s++)
 	{
 		c++;
 	}
-	return c;
+	return (c);
 }
 
