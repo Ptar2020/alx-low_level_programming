486b9c7a6f1e8317a2b511f67f0c4ab898f88c53
Peter Okoth Olwande <peterolwande@gmail.com>
2022-07-11 10:21:45 +0300

    fizz_buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -12,23 +12,23 @@ int main(void)
 	int x = 1;
 
 	for (x = 1; x <= 100; x++)
+	{
+		if (x % 3 == 0 && x % 5 == 0)
 		{
-	        	if (x % 3 == 0 && x % 5 == 0)
-	        	{
-				printf("FizzBuzz,");
-			}
-			else if (x % 5 == 0)
-			{
-				printf("Buzz, ");
-			}
-			else if (x % 3 == 0)
-			{
-				printf("Fizz, ");
-			}
-			else
-			{
-				printf("%i, ",x);
-			}
+			printf("FizzBuzz ");
 		}
+			else if (x % 5 == 0)
+		{
+			printf("Buzz ");
+		}
+		else if (x % 3 == 0)
+		{
+			printf("Fizz ");
+		}
+		else
+		{
+			printf("%i, ", x);
+		}
+	}
 	return (0);
 }
