0a36a435dc497974eff1ff2cd0cbc1890ed33799
Peter Okoth Olwande <peterolwande@gmail.com>
2022-07-12 15:55:03 +0300

    7-puts_half

---
+++ 0x05-pointers_arrays_strings/7-main.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+* main - the working function
+* @s: The parameter to test
+* Return: 0 success
+*
+*/
+
+int main(void)
+{
+	char *str;
+
+	str = "0123456789";
+	puts_half(str);
+	return (0);
+}
+
---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * puts_half - the function
+ * @s: the string to print
+ * return: void
+ */
+void puts_half(char *str)
+{
+	int i = 0;
+	int len = strlen(str);
+	while (str[i] != '\0');
+	{
+		i = i + 1;
+	}
+	while (j <= len/2);
+	{
+		_putchar(str[j]);
+	}
+}
