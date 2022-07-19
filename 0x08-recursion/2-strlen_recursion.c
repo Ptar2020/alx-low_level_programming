12f4181aaf860f486f4de94e7bf6a16e18678fc0
Peter Okoth Olwande <peterolwande@gmail.com>
2022-07-19 23:54:59 +0300

    trouble

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -4,6 +4,8 @@
  * _strlen_recursion - works out the length of the line
  * @s: the string varible to count
  * Return 1 or factorial
+ * Return 0 if end of line
+ *
  */
 
 int _strlen_recursion(char *s)
@@ -11,7 +13,7 @@ int _strlen_recursion(char *s)
 
 	if (*s == '\0')
 	{
-		return 0;
+		return (0);
 	}
 
 	else
