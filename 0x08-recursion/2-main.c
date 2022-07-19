bacb47830cf4011f820b560dcfa5916d45539e4c
Peter Okoth Olwande <peterolwande@gmail.com>
2022-07-19 23:23:15 +0300

    2assignments

---
+++ 0x08-recursion/2-main.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - checks the code 
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int n;
+
+	n = _strlen_recursion("Corbin Colman");
+	printf("%d\n", n);
+	return (0);
+}
---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - works out the length of the line
+ * @s: the string varible to count
+ * @c: The counter for length of the string
+ * @str_len: the variable carrying the returned answer
+ * Return c
+ */
+
+int _strlen_recursiom(char *s)
+{
+	int c = 0;
+
+	if (s[c] != '\0')
+	{
+		int str_len = 1 + _strlen_recursion(s + 1);
+		return str_len;
+	}
+
+}
