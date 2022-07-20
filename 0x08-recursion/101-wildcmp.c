920821435813740bd4b7626a773f79d379620e7c
Peter Okoth Olwande <peterolwande@gmail.com>
2022-07-20 22:14:36 +0300

    similar strings

---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * wildcmp - function getting the answe
+ * @s1: The first string
+ * @s2: The second string
+ * return: an integer value
+ */
+
+int wildcmp(char *s1, char *s2)
+{
+	if (*s1 != *s2)
+	{
+		return (0);
+	}
+	else
+	{
+		return (1);
+	}
+	
+}
