24bff45f2faf2d5da547e2cd8272e6af77902158
Peter Okoth Olwande <peterolwande@gmail.com>
2022-07-25 06:08:27 +0300

    0-create_array

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -13,16 +13,14 @@ char *create_array(unsigned int size, char c)
 {
 	unsigned int i = 0;
 
-	char *ptr = (char *)malloc(size * sizeof(char));
+	char *ptr = malloc(size * sizeof(char));
+
 	if (size == 0)
 		return (NULL);
-	else
+	while (i < size)
 	{
-		while (i < size)
-		{
-			ptr[i] = c;
-			i = i + 1;
-		}
+		ptr[i] = c;
+		i = i + 1;
 	}
 
 	return (ptr);
