d506ab5832531588e685f69d74f045b2d7d34531
Peter Okoth Olwande <peterolwande@gmail.com>
2022-07-28 20:13:55 +0300

    attempts

---
+++ 0x0C-more_malloc_free/101-mul.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+	int num1 = 5, num2 = 6, mul;
+
+	mul = num1 * num2;
+
+	printf("The result of the multiplication is %i.", mul);
+	return (EXIT_SUCCESS);
+}
+
