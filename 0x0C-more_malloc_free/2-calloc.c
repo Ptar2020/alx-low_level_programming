576b22f3475f24144ea6405c5d3a866b46cf76ea
Peter Okoth Olwande <peterolwande@gmail.com>
2022-07-28 21:28:09 +0300

    2-calloc.c

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -11,24 +11,23 @@
   */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-int i = 0, l = 0;
-char *p;
+	int i = 0, l = 0;
+	char *p;
 
-if (nmemb == 0 || size == 0)
-return (NULL);
+	if (nmemb == 0 || size == 0)
+	return (NULL);
 
-l = nmemb * size;
-p = malloc(l);
+	l = nmemb * size;
+	p = malloc(l);
 
-if (p == NULL)
-return (NULL);
+	if (p == NULL)
+	return (NULL);
 
-while (i < l)
-{
-p[i] = 0;
-i++;
+	while (i < l)
+	{
+		p[i] = 0;
+		i++;
+	}
+
+	return (p);
 }
-
-return (p);
-}
-i
