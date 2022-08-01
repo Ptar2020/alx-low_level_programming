b66c8bddfa345effa5a219d90a064f5d359086bd
Peter Okoth Olwande <peterolwande@gmail.com>
2022-08-01 20:56:20 +0300

    all

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,16 @@
+#ifndef DOG
+#define DOG
+/**
+ * dog - The dog structure
+ * @name:The dog name
+ * @age:The dog's age
+ * @owner:The dog's owner
+ */
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-}
+};
+
+#endif
