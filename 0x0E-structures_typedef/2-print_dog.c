b97708eda1bebaf10b26a8d0fe53e39ecf1ba3ad
Peter Okoth Olwande <peterolwande@gmail.com>
2022-08-01 22:08:21 +0300

    task2

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,16 @@
+#include "dog.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * print_dog - The sructure of the dog
+ * @d: The structure of the dog
+ *
+ */
+
+void print_dog(struct dog *d)
+{
+
+
+
+}
--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,4 @@
 
-void init_dog(struct dog *d, char *name, float age, char *owner);
-
 /**
  * dog - The dog structure
  * @name:The dog name
@@ -14,3 +12,6 @@ struct dog
 	float age;
 	char *owner;
 };
+
+void print_dog(struct dog *d);
+void init_dog(struct dog *d, char *name, float age, char *owner);
