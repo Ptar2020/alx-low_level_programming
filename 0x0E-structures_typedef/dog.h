3bc8376f19029d7e1c939da21bcd8a5dc1ee9dd8
Peter Okoth Olwande <peterolwande@gmail.com>
2022-08-01 22:15:58 +0300

    my work

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,4 +1,4 @@
-
+#ifndef MAIN
 /**
  * dog - The dog structure
  * @name:The dog name
@@ -15,3 +15,5 @@ struct dog
 
 void print_dog(struct dog *d);
 void init_dog(struct dog *d, char *name, float age, char *owner);
+
+#endif
