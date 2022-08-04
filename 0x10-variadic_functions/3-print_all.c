f5d7dc1651281cca5988b413e2298ff2d13c5eb8
Peter Okoth Olwande <peterolwande@gmail.com>
2022-08-04 20:44:08 +0300

    3-main.c

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,17 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * print_all - The function printing all the items
+ * @format: List of arguments
+ * Return: Nothing to return
+ */
+
+void print_all(const char * const format, ...)
+
+{
+
+}
+
