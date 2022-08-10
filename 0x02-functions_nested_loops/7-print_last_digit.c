89f310c0e30963ac3b6c78fe90826d5c74a8f846
Ptar* <62691985+Ptar2020@users.noreply.github.com>
2022-08-10 09:03:39 +0300

    Last digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,14 +1,24 @@
 #include "main.h"
+
 /**
-* print_last_digit -  main for this function
-*
-* @a: the number to be tested
-* last: the variable container for the answer.
-* Return : The expected output
-*/
-int print_last_digit(int a)
+  * print_last_digit - Prints the last digit of a number
+  * @n: The number to be treated
+  *
+  * Return: Value of the last digit of number
+  */
+int print_last_digit(int n)
 {
-int last;
-last = a % 10;
-return (last);
+	int last;
+
+	last = n % 10;
+
+	if (last < 0)
+	{
+		last = last * -1;
+	}
+
+	_putchar(last + '0');
+
+	return (last);
 }
+
