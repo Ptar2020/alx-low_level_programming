86d44292654f3f1bcd6265c8893e18bfa2c1e430
Peter Okoth Olwande <peterolwande@gmail.com>
2022-08-18 13:27:17 -0700

    Get endianness

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -1,9 +1,13 @@
 #include <stdio.h>
 #include "main.h"
- 
+/**
+ * get_endianness - the working function
+ * Return: 0 or 1
+ */
 int get_endianness(void)
 {
-        short int word = 0x0110;
-        char *b = (char *)&word;
-        return (b[0] ? 1 : 0);
+	short int word = 0x0110;
+	char *b = (char *)&word;
+
+	return (b[0] ? 1 : 0);
 }
