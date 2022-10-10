5479d73de09b53411dcd522da4d1cb7eb21b8dfb
Ptar2020 <peterolwande@gmail.com>
2022-10-10 23:09:59 +0300

    No

--- 0x18-dynamic_libraries/main.h
+++ 0x18-dynamic_libraries/main.h
@@ -1,6 +1,7 @@
-#ifndef _SHARED_LIB_H_
-#define _SHARED_LIB_H_
+#ifndef _MAIN_
+#define _MAIN_
 
+#include <stdio.h>
 #include <stdlib.h>
 
 int _putchar(char c);
@@ -11,9 +12,11 @@ int _isupper(int c);
 int _isdigit(int c);
 int _strlen(char *s);
 void _puts(char *s);
-int _atoi(char *s);
+char *_strcpy(char *dest, char *src);
+int _atoi(void);
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
+char *_strncpy(char *dest, char *src, int n);
 int _strcmp(char *s1, char *s2);
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
@@ -22,4 +25,4 @@ unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
 
-#endif
+#endif 
